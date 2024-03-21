d10d5d50f5f99152b93bbd142aedf23529b82d2d
Átila Costa <atilarebolo@gmail.com>
2024-03-21 20:47:53 -0300

    começando amexer na section novidades

--- src/app/Novidades/novidades.tsx
+++ src/app/Novidades/novidades.tsx
@@ -5,23 +5,23 @@ const Novidades = () => {
     return (
         <>
             <div className="content">
-                <section >
+                <section className="novidades" >
                     <h2>Veja o que há de novo em nosso portal</h2>
-                    <div >
+                    <div className="area_cards">
 
-                        <div >
+                        <div className="cards">
                             <Image src="" alt=""></Image>
                             <p>explicações mais simples para nossos produtos</p>
                             <Link href="#">Mais informações</Link>
                         </div>
 
-                        <div >
+                        <div className="cards">
                             <Image src="" alt=""></Image>
                             <p>Apresentando o Novo Mascote e a nova Área de Acessibilidade Salesforce</p>
                             <Link href="#">Mais informações</Link>
                         </div>
 
-                        <div >
+                        <div className="cards">
                             <Image src="" alt=""></Image>
                             <p>Apresentando o Novo Mascote e a nova Área de Acessibilidade Salesforce</p>
                             <Link href="#">Mais informações</Link>
