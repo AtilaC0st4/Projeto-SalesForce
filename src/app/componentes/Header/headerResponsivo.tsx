7731c9d665461e089a0d8b5319611eb5e357db95
Atila <atilarebolo@gmail.com>
2024-04-30 18:03:04 -0300

    Estilizando o sidebar

--- src/app/componentes/Header/headerResponsivo.tsx
+++ src/app/componentes/Header/headerResponsivo.tsx
@@ -6,11 +6,11 @@ import Image from 'next/image';
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-if (isOpen == true){
-var img = '/imagens/contraste.png'
-}else {
- var  img = '/imagens/Logo.png'
-}
+  if (isOpen == true) {
+    var img = '/imagens/close.png'
+  } else {
+    var img = '/imagens/menu.png'
+  }
 
   const toggle = () => {
     setIsOpen(!isOpen);
@@ -20,39 +20,35 @@ var img = '/imagens/contraste.png'
     <>
 
       <header className='sidebar'>
-        
+        <div className="content" >
 
-      <a href="./"><Image src={img} alt="Logo salesforce" width={80} height={60}></Image></a>
+          <div className='sidebar_container'>
 
-        <button onClick={toggle}>
-          {/* Imagem ou ícone para abrir o menu */}
-          <img src="/path/to/your/image.png" alt="Open Menu" />
-        </button>
+          <a href="./"><Image src="/imagens/Logo.png" alt="Logo salesforce" width={80} height={60}></Image></a>
 
-        <div
-          className={`sidebar ${isOpen ? 'open' : 'closed'}`}
-          style={{
-            opacity: `${isOpen ? "1" : "0"}`,
-            top: `${isOpen ? "0" : "-100%"}`,
-          }}
-        >
-          <button onClick={toggle}>
-            {/* Ícone para fechar o menu */}
-            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
-              <path fill="currentColor" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" />
-            </svg>
+          <button className='btnSidebar' onClick={toggle}>
+            {/* Imagem ou ícone para abrir o menu */}
+            <img src={img} alt="Open Menu" />
           </button>
+          </div>
+          <nav
 
+            className={`sidebar ${isOpen ? 'open' : 'closed'}`}
+            style={{
+              opacity: `${isOpen ? "1" : "0"}`,
+              top: `${isOpen ? "0" : "-100%"}`,
+            }}
+          >
 
-
-          <ul>
-            <li>
-              <Link href="#produtos" >Produtos</Link>
-            </li>
-            <li>
-              <Link href="#noticias" >Notícias</Link>
-            </li>
-          </ul>
+            <ul>
+              <li>
+                <Link href="#produtos" >Produtos</Link>
+              </li>
+              <li>
+                <Link href="#noticias" >Notícias</Link>
+              </li>
+            </ul>
+          </nav>
         </div>
 
       </header>
