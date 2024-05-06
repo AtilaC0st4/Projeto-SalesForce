6459c4cb1c6d5cb1f009b07f96e48859c1437de5
Átila Costa <atilarebolo@gmail.com>
2024-05-06 12:21:18 -0300

    faznedo uma area de login

--- src/app/pageLogin/page.tsx
+++ src/app/pageLogin/page.tsx
@@ -7,13 +7,19 @@ const Login: React.FC = () => {
 
     return(
         <>
-            <section>
+            <section className="login" >
 
-                
+                <div className="area_login">
+
+                    <div className="txt_login">LOgo</div>
+                    <div className="inputs_login">login</div>
+
+                </div>
 
             </section>
             
 
         </>
     )
-}
+} 
+export default Login;
